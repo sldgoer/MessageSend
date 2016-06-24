30b466d795ce6b822a46b91d9575cf71aad1d9e8
sldgoer <13500288851@163.com>
2016-06-24 14:21:40 +0800

    Add try/catch

--- JMGJJSMS/SMSSend.cs
+++ JMGJJSMS/SMSSend.cs
@@ -29,26 +29,49 @@ namespace JMGJJSMS
             Console.WriteLine("ini res:" + res.ToString());
         }
 
-        public int SendMessage(string mobile, string content, long smid)
+        public int SendMessage(string mobile, string content, long smid) 
         {
-            return apiclient.sendSM(mobile, content, smid);
+            try
+            {
+                return apiclient.sendSM(mobile, content, smid);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
         public int SendMessage(string[] mobiles, string content, long smid)
         {
-            return apiclient.sendSM(mobiles, content, smid);
+            try
+            {
+                return apiclient.sendSM(mobiles, content, smid);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
         //public int SendMessage()
 
         public List<RptResult> RecieveRPT(long smid)
-        {            
-            var rpts=apiclient.receiveRPT(smid, -1);
-            List<RptResult> list = new List<RptResult>();
-            foreach (var r in rpts)
+        {
+            try
             {
-                list.Add(new RptResult(mobile: r.getMobile(), smid: r.getSmID(), code: r.getCode(), desc: r.getDesc(), rpttime: r.getRptTime()));
+                var rpts = apiclient.receiveRPT(smid, -1);
+                List<RptResult> list = new List<RptResult>();
+                if (rpts.Length > 0)
+                {
+                    foreach (var r in rpts)
+                    {
+                        list.Add(new RptResult(mobile: r.getMobile(), smid: r.getSmID(), code: r.getCode(), desc: r.getDesc(), rpttime: r.getRptTime()));
+                    }
+                }
+                return list;
             }
-            return list;
-            
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
     }
